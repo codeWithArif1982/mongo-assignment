ca4d668d5d40041c92643d7b79605435d45f63f3
codeWithArif1982 <codewitharif1982@gmail.com>
2023-06-08 00:25:22 -0400

    Code update

---
+++ models/products.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Products = mongoose.model('Products', productSchema);
+
+module.exports = Products;
